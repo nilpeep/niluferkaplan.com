dd0d1b9041faa5240c7e3912a506e9e54720b9ab
nilpeep <niluferk038@gmail.com>
2024-03-11 17:12:41 +0300

    navbar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import Navbar from './components/Navbar';
+import './index.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    
+      <Navbar/>
+    <div className="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-md flex items-center space-x-4">
+      <div>
+        <div className="text-xl font-medium text-black">ChitChat</div>
+        <p className="text-sky-500">You have a new message!</p>
+      </div>
     </div>
+    </>
   );
 }
 
-export default App;
+export default App
+
---
+++ tailwind.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}'], // Projenizdeki Tailwind sınıflarının kullanıldığı dosyaları belirtir.
+  theme: {
+    extend: {}, // Tema ayarlarını özelleştirmek için kullanılır.
+  },
+  plugins: [], // Tailwind CSS plugin'lerini burada belirtebilirsiniz.
+}
