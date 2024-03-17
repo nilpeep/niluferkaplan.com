3e0eb537d30313e2d323a257e0b07ec678f07a54
nilpeep <niluferk038@gmail.com>
2024-03-17 23:16:11 +0300

    up

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,18 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{js,jsx,ts,tsx}'], // Projenizdeki Tailwind sınıflarının kullanıldığı dosyaları belirtir.
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
   theme: {
-    extend: {}, // Tema ayarlarını özelleştirmek için kullanılır.
+    extend: {
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+    },
   },
-  plugins: [], // Tailwind CSS plugin'lerini burada belirtebilirsiniz.
-}
+  plugins: [],
+};
