8051c24fc907cac991d83439e793d1d603a937d7
nilpeep <niluferk038@gmail.com>
2024-04-04 03:06:19 +0300

    up

--- src/app/page.js
+++ src/app/page.js
@@ -6,27 +6,24 @@ export default function Home() {
     <>
       <div className={styles.container}>
         <div className={styles.about}>
-          <h1 className={styles.title}>Backend developer.</h1>
-          <div className={styles.content}>
-            <div className={styles.textContainer}>
-              <p className={styles.description}>
-                I am a backend developer with a passion for creating efficient
-                and scalable web applications. I have experience with Node.js,
-                Express.js, and MongoDB. I am also proficient with React.js and
-                Next.js. I am a quick learner and collaborate closely with
-                clients to create efficient, scalable, and user-friendly
-                solutions.
-              </p>
-              <div className={styles.buttons}>
-                <button className={styles.button}>Contact me</button>
-                <button className={styles.button}>View blog</button>
-                <button className={styles.button}>View resume</button>
-                <button className={styles.button}>View portfolio</button>
-              </div>
+          <div className={styles.textContainer}>
+            <h1 className={styles.title}>Backend developer.</h1>
+            <p className={styles.description}>
+              I am a backend developer with a passion for creating efficient and
+              scalable web applications. Want to learn more? Check out
+            </p>
+            <div className={styles.buttons}>
+              <button className={styles.button}>Contact me</button>
+              <button className={styles.button}>View blog</button>
+              <button className={styles.button}>View resume</button>
+              <button className={styles.button}>View portfolio</button>
             </div>
-            <div className={styles.imgContainer}>
-              <Image src="/banner1.jpeg" objectFit="contain" fill={true} />
-            </div>
+          </div>
+          <div className={styles.empty}>
+
+          </div>
+          <div className={styles.imgContainer}>
+            <Image className={styles.img} src="/banner1.jpeg" fill={true} />
           </div>
         </div>
         <div className={styles.blogs}>
