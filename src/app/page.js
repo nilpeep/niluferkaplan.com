0a04b7259935cde50646a98f1f9a8651835f9922
nilpeep <niluferk038@gmail.com>
2024-04-04 22:12:15 +0300

    buttons

--- src/app/page.js
+++ src/app/page.js
@@ -7,23 +7,20 @@ export default function Home() {
       <div className={styles.container}>
         <div className={styles.about}>
           <div className={styles.textContainer}>
-            <h1 className={styles.title}>Backend developer.</h1>
+            <h1 className={styles.title}>Software developer.</h1>
             <p className={styles.description}>
-              I am a backend developer with a passion for creating efficient and
-              scalable web applications. Want to learn more? Check out
+              Software developer with a passion for creating efficient and
+              scalable web applications.<br></br> im currently specializing on
+              MERN-stack.<br></br>
+              <br></br> Want to learn more? Check out
             </p>
             <div className={styles.buttons}>
               <button className={styles.button}>Contact me</button>
-              <button className={styles.button}>View blog</button>
-              <button className={styles.button}>View resume</button>
-              <button className={styles.button}>View portfolio</button>
+              <button className={styles.button}>Learn more</button>
             </div>
           </div>
-          <div className={styles.empty}>
-
-          </div>
           <div className={styles.imgContainer}>
-            <Image className={styles.img} src="/banner1.jpeg" fill={true} />
+            <Image className={styles.img} src="/blueBanner.jpeg" fill={true} />
           </div>
         </div>
         <div className={styles.blogs}>
