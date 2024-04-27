4830f150678ca638a48c7b46664b9d7553eb0f82
nilpeep <niluferk038@gmail.com>
2024-04-27 10:58:13 +0300

    up

--- src/components/contact/page.jsx
+++ src/components/contact/page.jsx
@@ -8,9 +8,12 @@ const ContactModal = ({ isOpen, onClose, children }) => {
   if (!isOpen) return null;
   return (
     <div style={{ zIndex:'9', position: 'fixed', top: 0, left: 0, right: 0, bottom: 0, backgroundColor: 'rgba(0, 0, 0, 0.5)' }} onClick={onClose}>
-      <div style={{ width: '500px', margin: '100px auto', backgroundColor: 'white', padding: '20px', borderRadius: '8px', position: 'relative',padding:'50px' }} onClick={e => e.stopPropagation()}>
+      <div style={{ maxWidth: '400px', margin: '100px auto', backgroundColor: 'white', padding: '20px', borderRadius: '8px', position: 'relative',padding:'50px' }} onClick={e => e.stopPropagation()}>
         <form className={styles.form}>
-          <p>Your thoughts are important to me</p>
+          <div className={styles.header}>
+          <Image className={styles.img} src='/about.png' alt='nil' width={90} height={90}/>
+          <p className={styles.text}>You are reaching<br></br>Nilufer Kaplan</p>
+          </div>
           <input type="text" placeholder='Your name' />
           <input type="text" placeholder='Your email' />
           <input type="text" placeholder='Your phone number (optional)' />
